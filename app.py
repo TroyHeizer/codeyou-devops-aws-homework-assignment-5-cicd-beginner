e3952e910c493d69364c067c896e8d531e630033
TroyHeizer <TroyHeizer@gmail.com>
2025-01-07 22:58:24 -0500

    fixed first "error"

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -6,9 +7,11 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Welcome to the Flask App!"})
 
+
 @app.route("/health")
 def health():
     return jsonify({"status": "OK"}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
